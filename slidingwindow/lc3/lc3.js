2be3be39d735f8da40d0311ba03516fc9eefbef8
jacgit18 <jacdevgoo2020@gmail.com>
2021-07-21 19:00:32 -0400

    added challenge

---
+++ slidingwindow/lc3/lc3.js
@@ -0,0 +1,20 @@
+// Longest Substring Without Repeating Characters
+
+// Given a string s, find the length of the longest substring without repeating characters.
+
+
+let lengthOfLongestSubstring = (s) =>{
+
+    
+};
+
+
+let edgecase1 = "abcabcbb"; // 3
+let edgecase2 = "bbbbb"; // 1
+let edgecase3 = "pwwkew"; // 3
+let edgecase4 = ""; // 0
+
+console.log(`${lengthOfLongestSubstring(edgecase1)}`);
+console.log(`${lengthOfLongestSubstring(edgecase2)}`);
+console.log(`${lengthOfLongestSubstring(edgecase3)}`);
+console.log(`${lengthOfLongestSubstring(edgecase4)}`);
