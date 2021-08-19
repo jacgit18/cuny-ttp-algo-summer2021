72b449cda99be1f11f28eb4b46097d37def49d11
jacgit18 <jacdevgoo2020@gmail.com>
2021-08-19 18:54:20 -0400

    started review

--- twopointers/lc16/lc16.js
+++ twopointers/lc16/lc16.js
@@ -16,19 +16,62 @@
  * 
  */
 
-const triplet_sum_close_to_target =  (arr, target_sum) =>{
+const triplet_sum_close_to_target =  (arr, targetSum) =>{
+  // console.log(arr)
 
-  // test cases
+  arr.sort((a, b) => {
+    a-b;
+
+    // console.log(`Sort  ${a - b}`)
+    // console.log(a,b)
+    console.log(arr)
 
 
-  return -1;
+  });
+  // console.log(" ")
+
+  let smallest_difference = Infinity;
+
+  for (let i = 0; i < arr.length - 2; i++) {
+    let left = i + 1,
+      right = arr.length - 1;
+
+      // console.log(`Left ${left[arr]}`)
+      // console.log(`Right ${right}`)
+
+    while (left < right) {
+      const target_diff = targetSum - arr[i] - arr[left] - arr[right];
+
+      if (target_diff === 0) { // we've found a triplet with an exact sum
+        return targetSum - target_diff; // return sum of all the numbers
+      }
+  
+      if (Math.abs(target_diff) < Math.abs(smallest_difference)) {
+        smallest_difference = target_diff; // save the closest difference
+      }
+      // the second part of the following 'if' is to handle the smallest sum when we have more than one solution
+      if (Math.abs(target_diff) < Math.abs(smallest_difference) ||
+        (Math.abs(target_diff) === Math.abs(smallest_difference) && target_diff > smallest_difference)) {
+        smallest_difference = target_diff; // save the closest and the biggest difference
+      }
+  
+      if (target_diff > 0) {
+        left += 1; // we need a triplet with a bigger sum
+      } else {
+        right -= 1; // we need a triplet with a smaller sum
+      }
+
+      
+    }
+  }
+  return targetSum - smallest_difference;
+
 };
 
-console.log(triplet_sum_close_to_target([-2, 0, 1, 2], 2));
-
-console.log(`Edge Cases`)
-console.log(triplet_sum_close_to_target([-3, -1, 1, 2], 1));
-console.log(triplet_sum_close_to_target([1, 0, 1, 1], 100));
+console.log(triplet_sum_close_to_target([-2, 0, 1, 2], 2))
+// console.log(`Edge Cases`)
+// console.log(triplet_sum_close_to_target([-3, -1, 1, 2], 1));
+// console.log(triplet_sum_close_to_target([1, 0, 1, 1], 100));
 
 
 
