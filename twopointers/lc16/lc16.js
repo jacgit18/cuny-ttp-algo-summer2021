e1291684c49ab41967543b148e25ae01b3fc0571
jacgit18 <jacdevgoo2020@gmail.com>
2021-08-20 15:12:51 -0400

    edit

--- twopointers/lc16/lc16.js
+++ twopointers/lc16/lc16.js
@@ -24,17 +24,17 @@ const triplet_sum_close_to_target =  (arr, targetSum) =>{
 
     // console.log(`Sort  ${a - b}`)
     // console.log(a,b)
-    console.log(arr)
+    // console.log(arr)
 
 
   });
   // console.log(" ")
 
-  let smallest_difference = Infinity;
+  let closest_difference = Infinity;
 
   for (let i = 0; i < arr.length - 2; i++) {
-    let left = i + 1,
-      right = arr.length - 1;
+    let left = i + 1; // always next value
+    let right = arr.length - 1;
 
       // console.log(`Left ${left[arr]}`)
       // console.log(`Right ${right}`)
@@ -46,13 +46,13 @@ const triplet_sum_close_to_target =  (arr, targetSum) =>{
         return targetSum - target_diff; // return sum of all the numbers
       }
   
-      if (Math.abs(target_diff) < Math.abs(smallest_difference)) {
-        smallest_difference = target_diff; // save the closest difference
+      if (Math.abs(target_diff) < Math.abs(closest_difference)) {
+        closest_difference = target_diff; // save the closest difference
       }
       // the second part of the following 'if' is to handle the smallest sum when we have more than one solution
-      if (Math.abs(target_diff) < Math.abs(smallest_difference) ||
-        (Math.abs(target_diff) === Math.abs(smallest_difference) && target_diff > smallest_difference)) {
-        smallest_difference = target_diff; // save the closest and the biggest difference
+      if (Math.abs(target_diff) < Math.abs(closest_difference) ||
+        (Math.abs(target_diff) === Math.abs(closest_difference) && target_diff > closest_difference)) {
+        closest_difference = target_diff; // save the closest and the biggest difference
       }
   
       if (target_diff > 0) {
@@ -61,10 +61,9 @@ const triplet_sum_close_to_target =  (arr, targetSum) =>{
         right -= 1; // we need a triplet with a smaller sum
       }
 
-      
     }
   }
-  return targetSum - smallest_difference;
+  return targetSum - closest_difference;
 
 };
 
@@ -87,7 +86,7 @@ console.log(triplet_sum_close_to_target([-2, 0, 1, 2], 2))
 // Solution
 // -----
 // arr.sort((a, b) => a - b);
-// let smallest_difference = Infinity;
+// let closest_difference = Infinity;
 // for (let i = 0; i < arr.length - 2; i++) {
 //   let left = i + 1,
 //     right = arr.length - 1;
@@ -97,13 +96,13 @@ console.log(triplet_sum_close_to_target([-2, 0, 1, 2], 2))
 //       return targetSum - target_diff; // return sum of all the numbers
 //     }
 
-//     if (Math.abs(target_diff) < Math.abs(smallest_difference)) {
-//       smallest_difference = target_diff; // save the closest difference
+//     if (Math.abs(target_diff) < Math.abs(closest_difference)) {
+//       closest_difference = target_diff; // save the closest difference
 //     }
 //     // the second part of the following 'if' is to handle the smallest sum when we have more than one solution
-//     if (Math.abs(target_diff) < Math.abs(smallest_difference) ||
-//       (Math.abs(target_diff) === Math.abs(smallest_difference) && target_diff > smallest_difference)) {
-//       smallest_difference = target_diff; // save the closest and the biggest difference
+//     if (Math.abs(target_diff) < Math.abs(closest_difference) ||
+//       (Math.abs(target_diff) === Math.abs(closest_difference) && target_diff > closest_difference)) {
+//       closest_difference = target_diff; // save the closest and the biggest difference
 //     }
 
 //     if (target_diff > 0) {
@@ -113,7 +112,7 @@ console.log(triplet_sum_close_to_target([-2, 0, 1, 2], 2))
 //     }
 //   }
 // }
-// return targetSum - smallest_difference;
+// return targetSum - closest_difference;
 
 // -----
 
