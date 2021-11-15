c7f47bc0a78fe0c89d7a8f6472ca506151d50c3a
jacgit18 <jacdevgoo2020@gmail.com>
2021-11-15 18:32:25 -0500

    mv files

---
+++ Code/_Algo/subsets/Python/lc78.py
@@ -0,0 +1,14 @@
+# Problem Statement #
+
+
+def find_permutations(nums):
+  result = []
+  # TODO: Write your code here
+  return result
+
+
+def main():
+  print("Here are all the permutations: " + str(find_permutations([1, 3, 5])))
+
+
+main()
