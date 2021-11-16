f75c7c1609b75a45e77eb743818ad4cd574ba28d
Joshua Carpentier <joshuacarpentier21@gmail.com>
2021-11-16 18:40:08 -0500

    Delete Code/test/Heaps Test  directory

--- Code/test/Heaps Test /lc.test.js
+++
@@ -1,7 +0,0 @@
-const lengthOfLongestSubstring = require('../../_Algo/slidingwindow/lc3/lc3');
-  
-  test('edgecase1', () => {
-    const edgecase1 = lengthOfLongestSubstring("abcabcbb");
-    
-    expect(edgecase1).toEqual(3);
-  });
