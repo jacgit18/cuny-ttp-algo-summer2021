665ada4e04b904dd0774c133b2e0b5be8041a7c6
jacgit18 <jacdevgoo2020@gmail.com>
2021-11-16 13:11:50 -0500

    edit python

--- Code/_Grokking_Algo/Top K Elements/Python/lc.py
+++ Code/_Grokking_Algo/Top K Elements/Python/lc.py
@@ -1,12 +1,47 @@
-// Given an array of integers nums which is sorted in ascending order, and an integer target, write a function 
-// to search target in nums. If target exists, then return its index. Otherwise, return -1.
+# Problem Statement #
+# Given an array of sorted numbers and a target sum, find a pair in the array whose sum is equal to the given target.
 
-// You must write an algorithm with O(log n) runtime complexity.
+# Write a function to return the indices of the two numbers (i.e. the pair) such that they add up to the given target.
+
+def pair_with_targetsum(arr, target_sum):
+
+
+  def main():
+    print(pair_with_targetsum([1, 2, 3, 4, 6], 6))
+    print(pair_with_targetsum([2, 5, 9, 11], 11))
+
+
+  main()
 
 
 
-const search = (nums, target) =>{
-    
-};
 
-console.log(search([-1,0,3,5,9,12], 9)); // output index were 9 is 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Solution
+# -----
+#   left, right = 0, len(arr) - 1
+#   while(left < right):
+#     current_sum = arr[left] + arr[right]
+#     if current_sum == target_sum:
+#       return [left, right]
+
+#     if target_sum > current_sum:
+#       left += 1  # we need a pair with a bigger sum
+#     else:
+#       right -= 1  # we need a pair with a smaller sum
+#   return [-1, -1]
