e94c3adfde87a50ec6d3d5d154644e2aaca0faee
jacgit18 <jacdevgoo2020@gmail.com>
2021-11-16 13:12:26 -0500

    edit python

--- Code/_Grokking_Algo/twopointers/Python/lc1.py
+++ Code/_Grokking_Algo/twopointers/Python/lc1.py
@@ -6,12 +6,12 @@
 def pair_with_targetsum(arr, target_sum):
 
 
-def main():
-  print(pair_with_targetsum([1, 2, 3, 4, 6], 6))
-  print(pair_with_targetsum([2, 5, 9, 11], 11))
+  def main():
+    print(pair_with_targetsum([1, 2, 3, 4, 6], 6))
+    print(pair_with_targetsum([2, 5, 9, 11], 11))
 
 
-main()
+  main()
 
 
 
