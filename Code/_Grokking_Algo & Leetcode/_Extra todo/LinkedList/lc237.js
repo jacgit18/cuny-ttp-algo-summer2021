f420c43cfaccb6c0c506fca2fc845e6a3f181638
jacgit18 <jacdevgoo2020@gmail.com>
2021-11-22 00:17:20 -0500

    mv files

---
+++ Code/_Grokking_Algo & Leetcode/_Extra todo/LinkedList/lc237.js
@@ -0,0 +1,26 @@
+const LinkedList = require("../../../_DataStructuresClasses/DataStrucFactoryInterview");
+const myLK = new LinkedList();
+
+let LinkListArray = [9,1,5,4];
+let head = myLK.ArrayLinkListDeserialize(LinkListArray)
+
+//  console.log(head); // full list
+//   console.log(head.value); // just 4 for no list
+//   console.log(head.next.value); // return 5
+//   console.log(head.next.next.value); // return 1
+//   console.log(head.next.next.next.value); // return 9
+//   console.log(head.next.next.next); // return 9
+//   console.log(head.next.next.next.next); // return null
+
+
+let node = head; 
+
+let deleteNode = function (node) {
+ 
+
+
+};
+
+deleteNode(node);
+
+
