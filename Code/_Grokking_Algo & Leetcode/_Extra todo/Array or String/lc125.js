46f327685eb865b6f122b1ba7ce0979b433f2ca5
jacgit18 <jacdevgoo2020@gmail.com>
2022-01-06 16:52:21 -0500

    added a chalenge

---
+++ Code/_Grokking_Algo & Leetcode/_Extra todo/Array or String/lc125.js
@@ -0,0 +1,10 @@
+const isPalindrome = (s) =>{
+
+}
+
+
+console.log(isPalindrome("A man, a plan, a canal: Panama"));
+
+try {
+  module.exports = isPalindrome;
+} catch (error) {}
---
+++ Code/_Grokking_Algo & Leetcode/_Extra todo/Array or String/lc20.js
@@ -0,0 +1,10 @@
+const isValid = (s) =>{
+
+}
+
+
+console.log(isValid("()"));
+
+try {
+  module.exports = isValid;
+} catch (error) {}
