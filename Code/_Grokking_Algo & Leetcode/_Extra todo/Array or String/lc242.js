ab36572f2b884c134222aceb85c37e66b511a9b7
jacgit18 <jacdevgoo2020@gmail.com>
2022-01-09 01:12:15 -0500

    added comments and ex

--- Code/_Grokking_Algo & Leetcode/_Extra todo/Array or String/lc242.js
+++ Code/_Grokking_Algo & Leetcode/_Extra todo/Array or String/lc242.js
@@ -1,9 +1,21 @@
 // Given a string s, return true if it is a palindrome, or false otherwise.
 
+// short version
+// var isAnagram = (s, t) => s.split('').sort().join('') === t.split('').sort().join('');
 
+// Build sorted array (char[] ) of s and t
+// Compare poped element from a and b array
+// After loop return true if there are no remaining in a or b else return false
 
 const isAnagram = (s, t) =>{
-
+  const a = s.split('').sort(),
+        b = t.split('').sort();
+    while (a.length && b.length) {
+        if (a.pop() !== b.pop()) {
+            return false;
+        }
+    }
+    return a.length || b.length ? false : true;
 }
 
 
