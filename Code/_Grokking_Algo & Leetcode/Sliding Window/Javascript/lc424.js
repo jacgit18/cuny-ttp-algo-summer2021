965cf626ffbf994b31b400d693d61dae772bc606
jacgit18 <jacdevgoo2020@gmail.com>
2022-01-15 14:15:09 -0500

    edit

--- Code/_Grokking_Algo & Leetcode/Sliding Window/Javascript/lc424.js
+++ Code/_Grokking_Algo & Leetcode/Sliding Window/Javascript/lc424.js
@@ -47,12 +47,10 @@ const characterReplacement = (s,k) =>{
 
 
 console.log(characterReplacement("ABAB",2)); // 4
-// Explanation: Replace the two 'A's with two 'B's or vice versa.
+console.log(characterReplacement("AABABBA",1)); // 4
+// AABABBA
+// AABBBBA
 
-// Input: s = "AABABBA", k = 1
-// Output: 4
-// Explanation: Replace the one 'A' in the middle with 'B' and form "AABBBBA".
-// The substring "BBBB" has the longest repeating letters, which is 4.
 
 try {
   module.exports = characterReplacement;
