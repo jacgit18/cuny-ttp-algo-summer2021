d7890e2f96147272b86b1f0836dffdf68155582e
jacgit18 <jacdevgoo2020@gmail.com>
2022-01-14 20:08:24 -0500

    passed first test case

--- Code/_Grokking_Algo & Leetcode/Sliding Window/Javascript/lc424.js
+++ Code/_Grokking_Algo & Leetcode/Sliding Window/Javascript/lc424.js
@@ -8,6 +8,40 @@
 
 
 const characterReplacement = (s,k) =>{
+  let [windowStart, windowEnd] = [0, 0]
+  let [currentLetter, nextLetter] = [s[windowStart], s[windowStart + 1]]
+  let maxRepeatStringLength = 0;
+  let repeatString = ""
+
+
+  if(s.length === 0 || k < 1) return 0
+
+  while(windowStart < s.length && k !== 0){
+
+    if(currentLetter === nextLetter  ){
+      repeatString += currentLetter;
+      // repeatString.replace(repeatString.substring(''), currentLetter); 
+      maxRepeatStringLength = Math.max(maxRepeatStringLength, repeatString.length);
+      windowStart++;
+
+    }
+    // let nextLetter = s[windowStart + 1]
+    // let currentLetter = s[windowStart]
+    if(currentLetter !== nextLetter) {
+      currentLetter = nextLetter
+      // console.log(currentLetter, nextLetter)
+      k--;
+      windowEnd++;
+    }
+
+    // && windowStart === windowEnd
+   
+
+          // console.log(currentLetter, nextLetter)
+
+
+  }
+  return maxRepeatStringLength
 
 }
 
