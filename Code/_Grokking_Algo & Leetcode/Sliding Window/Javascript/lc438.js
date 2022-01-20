34dfbd0f6459b26400087f4399da4ff7bf5075cf
jacgit18 <jacdevgoo2020@gmail.com>
2022-01-20 15:19:50 -0500

    edit

--- Code/_Grokking_Algo & Leetcode/Sliding Window/Javascript/lc438.js
+++ Code/_Grokking_Algo & Leetcode/Sliding Window/Javascript/lc438.js
@@ -9,17 +9,56 @@
  * @param {* long string} s 
  * @param {* short string} p 
  * @returns [all start indices of short string anagram in long string ]
- * anagram a word or sequence of letters Rearrange in different order but contains same letters
+ * anagram a word or sequence of letters Rearrange in different order but contains same letters index returned order doesnt matter
  */
 const findAnagrams = (s,p) =>{
+  let [longString, shortString] = [s, p]
   let [ windowStart, windowEnd ] = [0,0];
-  let [stringOneSequenceLength, stringTwoSequenceLength] = [s.length, p.length];
-  if(stringOneSequenceLength || stringTwoSequenceLength){
+  let sequence = [];
+  let [longSequenceLength, shortSequenceLength] = [longString.length, shortString.length];
+  let Count = 0;
+
+
+  if(longSequenceLength === 0 || shortSequenceLength === 0){
     return [stringOneSequenceLength, stringTwoSequenceLength]
   }
 
+  while(windowEnd < longSequenceLength){
+    // let shortStringRange = shortString.charCodeAt(windowEnd) - 'c'.charCodeAt(0); // 97-99
+ 
 
-  return []
+    // if(windowEnd <= shortSequenceLength){
+    //   // longString[windowEnd]
+    //   console.log(shortString[windowEnd])
+
+    //   ++windowStart;
+    // }
+
+    // let caset = longString[windowStart].charCodeAt(windowStart) === shortString.charCodeAt(windowEnd);
+
+    if(longString.substring(windowStart, windowEnd).length  === shortSequenceLength &&  longString.charCodeAt(windowStart)){
+      sequence.push(windowStart)
+      // console.log(sequence);
+      ++windowStart;
+
+    }
+
+    // console.log(caset, caset.length)
+    // if(caset){
+    //        return [windowEnd]
+
+    // }
+   
+    ++windowEnd;
+
+  }
+
+// console.log(sequence)
+  return [sequence[windowEnd]];
+  // return sequence
+
+  // return sequence.indexOf(sequence[sequence.length - 1])
+  
 }
 
 
@@ -27,6 +66,8 @@ const findAnagrams = (s,p) =>{
 
 
 console.log(findAnagrams("cbaebabacd","abc")); //  [0,6]
+console.log(findAnagrams("abab","ab")); //  [0,1,2]
+
 
 // The substring with start index = 0 is "cba", which is an anagram of "abc".
 // The substring with start index = 6 is "bac", which is an anagram of "abc".
