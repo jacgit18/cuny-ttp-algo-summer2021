41bf98951829611f0a57ae5bb858011762ed50c6
jacgit18 <jacdevgoo2020@gmail.com>
2022-01-21 22:16:44 -0500

    added whiteboard

--- Code/_Grokking_Algo & Leetcode/Sliding Window/Javascript/lc438Condition.js
+++ Code/_Grokking_Algo & Leetcode/Sliding Window/Javascript/lc438Condition.js
@@ -8,17 +8,25 @@ const findAnagrams = (s, p) => {
          hash[c]++;
      }
  }
- 
+ // === considers type & value == considers only value
  let res = [];
  let left = 0;
  let right = 0;
+ let count = 0;
  for (right;right<s.length;right++) {
-     if (hash[s[right]]!=null) hash[s[right]]--;
-     if (hash[s[right]]==0) uniqueChars--;
-     if (uniqueChars==0) res.push(left);
-     if (right - left + 1 == p.length) {
-         if (hash[s[left]]!=null) hash[s[left]]++;
-         if (hash[s[left++]]==1) uniqueChars++;
+     if (hash[s[right]]!==null) hash[s[right]]--;
+     if (hash[s[right]]===0) uniqueChars--;
+     if (uniqueChars===0) res.push(left);
+     if (right - left + 1 === p.length) {
+         if (hash[s[left]]!==null) hash[s[left]]++;
+        //  console.log(count++, " sec " ,hash[s[left++]])
+        //  console.log(count++, " sec " ,s[left++])
+        //  console.log(count++, " sec " ,s[left])
+        //  console.log(count++, " sec " ,left++)
+
+
+
+         if (hash[s[left++]]===1) uniqueChars++;
      }
  }
  return res;
@@ -26,4 +34,5 @@ const findAnagrams = (s, p) => {
 
 
 console.log(findAnagrams("cbaebabacd","abc")); //  [0,6]
+console.log(findAnagrams("abab","ab")); //  [0,1,2]
 
