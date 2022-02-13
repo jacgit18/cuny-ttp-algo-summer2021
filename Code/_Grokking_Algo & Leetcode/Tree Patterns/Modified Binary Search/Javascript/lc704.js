b6f94a8c13c5bea212ffd0459d3bb217d48c0a2a
jacgit18 <jacdevgoo2020@gmail.com>
2022-02-13 07:17:56 -0500

    edit

--- Code/_Grokking_Algo & Leetcode/Tree Patterns/Modified Binary Search/Javascript/lc704.js
+++ Code/_Grokking_Algo & Leetcode/Tree Patterns/Modified Binary Search/Javascript/lc704.js
@@ -5,15 +5,24 @@ const treeGetter = new treeSetter();
 
 // You must write an algorithm with O(log n) runtime complexity.
 
-class Node {
-    constructor(data, left = null, right = null) {
-      this.data = data;
-      this.left = left;
-      this.right = right;
-    }
+// class Node {
+//     constructor(data, left = null, right = null) {
+//       this.data = data;
+//       this.left = left;
+//       this.right = right;
+//     }
+//   }
+
+
+  class Node
+  {
+      constructor(data) {
+         this.left = null;
+         this.right = null;
+         this.data = data;
+      }
   }
-
-
+   
 
 const search = (nums, target) =>{
     let lo = 0, hi = nums.length-1;
@@ -52,12 +61,50 @@ function traverse(nums,start,end){
 }
 
 
-let tree= sortedArrayToBST([-1,0,3,5,9,12]); 
+// let tree= sortedArrayToBST([-1,0,3,5,9,12]); 
+//                  alt
+//      3           -1
+//    /  \          /  \
+//  -1    9        0    3
+// / \  / \       / \  /
+//   0 5   12    5  9  12
 
-//      3
-//    /  \
-//  -1    9
-// / \  / \
-//   0 5   12
+let root;
+     
+// Function to insert nodes in level order
+function insertLevelOrder(arr, root, i)
+{
+    // Base case for recursion
+    if (i < arr.length) {
+        let temp = new Node(arr[i]);
+        root = temp;
+
+        // insert left child
+        root.left = insertLevelOrder(arr, root.left,
+                                         2 * i + 1);
+
+        // insert right child
+        root.right = insertLevelOrder(arr, root.right,
+                                           2 * i + 2);
+    }
+    return root;
+}
+
+// Function to print tree nodes in InOrder fashion
+function inOrder(root)
+{
+    if (root != null) {
+        inOrder(root.left);
+        console.log(root.data + " ");
+        inOrder(root.right);
+    }
+}
+ 
+// let arr = [-1,0,3,5,9,12];
+// root = insertLevelOrder(arr, root, 0);
+// console.log(root);
+// inOrder(root);
 console.log(search([-1,0,3,5,9,12], 9)); // output index were 9 is  so 4
 console.log(search([-1,0,3,5,9,12], 2)); // output no 2 so return - 1
+// console.log(search(root, 9)); // output index were 9 is  so 4 dont work
+// console.log(search(root, 2)); // output no 2 so return - 1
