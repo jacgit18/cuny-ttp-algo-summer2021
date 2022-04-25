9a83c9c38d802a6e5c40381a0760035d33363fd3
jacgit18 <jacdevgoo2020@gmail.com>
2022-04-25 00:44:03 -0400

    broke down graph pointers

--- Code/_DataStructuresBuiltUpClasses/Tree&Graph Traversal(BFS/dfsRec.js
+++ Code/_DataStructuresBuiltUpClasses/Tree&Graph Traversal(BFS/dfsRec.js
@@ -38,7 +38,7 @@ class Vertex {
     return vertices[0];
   }
 
-  let idGraphIndex = 8;
+  let idGraphIndex = 8; 
   let GraphRelationshipArray = [
     [0, 1],
     [1, 2],
@@ -49,9 +49,75 @@ class Vertex {
     [4, 6],
     [4, 7],
     [5, 6],
+   
   ];
 
+  // let idGraphIndex = 5; // anything lesser then 5 throws error and no full graph
+
+  // let GraphRelationshipArray = [
+  //   [1, 2],
+  //   [2, 3],
+  //   [3, 4],
+  //   [4, 1]
+   
+  // ];
+
   let graph1 =  ArrayGraphDeserialize(idGraphIndex, GraphRelationshipArray)
+
+  let graph = new Vertex(1)
+  // graph.edges[0] = 1
+  
+  // graph.edges[0] = new Vertex(1)
+  
+  graph.edges[0] = new Vertex(2)
+  
+  // graph.id.edges[0] =  new Vertex(2)
+  // graph.edges[0].id =  new Vertex(2)
+  
+  graph.edges[1] = new Vertex(3)
+  
+  graph.edges[2] = new Vertex(4)
+  graph.edges[2].edges[0] = graph.id
+  graph.edges[2].edges[1] = graph.edges[1].id
+
+  graph.edges[1].edges[0] = graph.edges[0].id
+  graph.edges[1].edges[1] = graph.edges[2].id
+
+  graph.edges[0].edges[0] = graph.edges[1].id
+  graph.edges[0].edges[1] = graph.id
+
+  // graph.id.edge = graph.edges[2].id// migth not need to point 1
+
+  // 1-----------2
+  // |           |            
+  // 4-----------3
+
+ 
+  console.log(graph.edges)
+
+  // console.log(graph.edges[0])
+  // console.log(graph.edges[1])
+  // console.log(graph.edges[2])
+
+
+  // let v1;
+  // let v2;
+
+  // for (let edge in graph.edges){
+    // console.log(edge)
+    // v1 = edge[0]; 
+    // v2 = edge[1]; 
+
+    // vertices[v1].edges.push(vertices[v2]); 
+    // vertices[v2].edges.push(vertices[v1]);
+    
+  // }
+  
+  
+  
+    console.log('Small Graph ',graph)
+    console.log('graph ONE Test', graph1)
+
 
 
 function dfsRec(vertex) {
@@ -92,9 +158,12 @@ function dfsRec(vertex) {
 //   ]
 
 
+
 // DFSRec: [
 //     3, 6, 5, 7,
 //     4, 2, 1, 0
 //   ]
  
+
+// console.log("DFSRec:", dfsRec(graph));  
   console.log("DFSRec:", dfsRec(graph1));  
