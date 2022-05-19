92ece31d5881bc6b37d84beacde5452a8c14bd92
jacgit18 <joshuaxcarpentier@gmail.com>
2022-05-19 08:56:05 -0400

    added dict

--- Code/pramp/FlattenaDictionary.js
+++ Code/pramp/FlattenaDictionary.js
@@ -1,31 +1,74 @@
 const dict = require("./dict");
 // import dict from './dict'; ??
 
+
+// function RecflattenDictionary(dict, emptyString, map){
+//   for (let prop in dict){
+    
+//      let value = prop
+
+//         if (!dict(value)):
+//             if (initialKey == null || initialKey == ""):
+//                 flatDictionary.put(key, value)
+//             else:
+//                 flatDictionary.put(initialKey + "." + key, value)
+//         else:
+//             if (initialKey == null || initialKey == "")
+//                 flattenDictionaryHelper(key, value, flatDictionary)
+//             else:
+//                 flattenDictionaryHelper(initialKey + "." + key, value, flatDictionary)
+//   }
+      
+  
+  
+// }
+
 function RecflattenDictionary(dict, emptyString, map) {
   let itersection = ".";
 
-  for (prop in dict) {
-    if (typeof dict[prop] == "object") {
-      RecflattenDictionary(dict[prop], emptyString, map);
-    } else {
-      if (typeof dict[prop] == "string") {
-        console.log(prop, dict[prop]);
-      }
-    }
-  }
-  //   map[key] = dict[key]
+  for (let prop in dict) {
+    let value =  dict[prop];
+    let dataType = typeof(value);
 
-  //   return map
+    if(dataType == 'boolean' || dataType == 'number' || dataType == 'string' || dataType == 'object' ){
+       if(emptyString == null || emptyString == ""){
+      map.set(prop, value)
+    }else{
+      map.set(emptyString + itersection + prop, value)
+
+    }
+    // console.log(map)
+      
+    } else{
+          if(emptyString == null || emptyString == "") {
+      // console.log(prop + "." )
+      // console.log(prop + "." )
+
+      RecflattenDictionary(value, prop, map);
+
+    }
+
+    else{
+      RecflattenDictionary(emptyString + itersection + prop, value, map)
+
+    }
+    }
+
+
+  }
+
+    // return map
 }
 
 function flattenDictionary(dict) {
   let emptyString = "";
-  let map = {};
-
-  return RecflattenDictionary(dict, emptyString, map);
+  let map = new Map;
+  RecflattenDictionary(dict, emptyString, map);
+  return map
 }
 
-flattenDictionary(dict);
+// flattenDictionary(dict);
+console.log(flattenDictionary(dict));
 
 //   output: {
 //     "Key1" : "1",
---
+++ Code/pramp/dict2.js
@@ -0,0 +1,44 @@
+const dict = require("./dict");
+
+
+function flattenDictionary(dict) {
+    // your code goes here
+    let results = {};
+    let currKey = "";
+  
+    function flattenUtil(dict) {
+      // 1. 'Iterate' over the dictionary
+      // Keys will either be 'Key_' for outermost level
+      // or a letter/empty string for inner levels
+      // 2. If the value is an int/string, append onto output dictionary
+      // else if the value is a dictionary, recurse
+      // 3. Base case: dict empty, return
+  
+      Object.keys(dict).forEach(key => {
+        if (typeof dict[key] === 'object') {
+          if (!currKey) {
+            currKey += key;
+          } else {
+            currKey += `.${key}`;
+          }
+          flattenUtil(dict[key]);
+        } else {
+          if (!currKey) {
+            results[`${key}`] = dict[key];
+          } else {
+            if (!key) {
+              results[`${currKey}`] = dict[key];
+            } else {
+              results[`${currKey}.${key}`] = dict[key];
+            }
+          }
+        }
+      });
+    }
+  
+    flattenUtil(dict);
+    return results;
+  }
+
+
+console.log(  flattenDictionary(dict));
